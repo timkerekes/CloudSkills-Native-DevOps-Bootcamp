4b9358d3f121c2ed1e26337839c26387bb92c572
timkerekes <timothy.kerekes@gmail.com>
2021-02-24 16:02:26 -0700

    Initial Cloud Native DevOps Bootcamp commit

---
+++ Week 2 - Scripting Like a Developer/s3bucket.py
@@ -0,0 +1,29 @@
+import boto3
+import sys
+
+try:
+    def main():
+        create_s3bucket(bucket_name)
+
+except Exception as e:
+    print(e)
+
+
+def create_s3bucket(bucket_name):
+    s3_bucket = boto3.client(
+        's3',
+    )
+
+    bucket = s3_bucket.create_bucket(
+        Bucket=bucket_name,
+        ACL='private',
+        CreateBucketConfiguration={'LocationConstraint': 'us-west-1'}
+    )
+
+    print(bucket)
+
+
+bucket_name = sys.argv[1]
+
+if __name__ == '__main__':
+    main()
